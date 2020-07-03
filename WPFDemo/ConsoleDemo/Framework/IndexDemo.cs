c0e2fa18f497096e44dff86a64864be4aec5e0a0
wfoolish <342358952@qq.com>
2020-07-03 13:51:23 +0800

    console测试项目

---
+++ WPFDemo/ConsoleDemo/Framework/IndexDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo.Framework
+{
+    public class IndexDemo
+    {
+
+
+        public object this[string key]
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
